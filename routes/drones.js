a34baaa5ed3d600ded6c27a3ffb9fce155b21b1c
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-13 17:43:53 +0100

    Iteration 3 solved

--- routes/drones.js
+++ routes/drones.js
@@ -21,13 +21,20 @@ router.get('/drones', async (req, res, next) => {
 });
 
 router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  res.render("drones/create-form.hbs")
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+router.post('/drones/create', async (req, res, next) => {
+  try {
+    await DroneModel.create({
+      name: req.body.name,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxSpeed
+    })
+    res.redirect("/drones")
+  } catch (err){
+    next(err)
+  }
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
