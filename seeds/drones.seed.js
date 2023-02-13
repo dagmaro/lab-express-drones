fd41100407744451c0557434206f3bc8234f4d60
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-13 17:10:39 +0100

    Iteration 1 solved

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,15 @@
 // Iteration #1
+require("../db")
+const DroneModel = require("../models/Drone.model")
+const drones = require("./drones.seed.json")
+
+async function inserData() {
+    try {
+        await DroneModel.insertMany(drones)
+        console.log("drones insertados")
+    } catch (err) {
+      console.log(err)
+    }
+}
+inserData()
+
